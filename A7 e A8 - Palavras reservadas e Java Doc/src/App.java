8a3afb2a1bcf95d86f38b3d8671b994d83d409eb
João Targino <joaoetargino@gmail.com>
2024-05-28 15:44:36 -0300

    class7and8: keywords and javadoc

---
+++ A7 e A8 - Palavras reservadas e Java Doc/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //No Java, existem 52 palavras reservadas. São classificadas em grupos e escritas com letras MINÚSCULAS. Normalmente, possuem cores epeciais nas IDE's
+
+        //Controle de pacotes:
+
+        // import e package
+
+        //Modificadores de acesso:
+
+        //public, private e protected
+
+        //Primitivos:
+
+        // boolean, byte, char, double, float, int, long, short e void
+
+        //Modificadores de classes, variáveis e métodos:
+
+        //abstract, class, extends, final, implements, interface, native, new, static, strictfp, synchronized, transient, volatile
+
+        //Controle de fluxo dentro de um bloco de código:
+
+        //break, case, continue...
+        System.out.println("Hello, World!");
+    }
+}
